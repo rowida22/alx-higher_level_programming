812171450134d0ee8fa81e2f54807e63fdb1f462
rowida22 <rowida91200@gmail.com>
2023-05-17 02:35:05 +0300

    2

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
---comment
+-- comment
 SELECT AVG(`score`) AS `average`
 FROM `second_table`;
--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
---comment
-SELECT COUNT('score') AS 'number'
-FROM second_table
-GROUP BY score
-ORDER BY score DESC;
+-- comment
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
