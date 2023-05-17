0a42214ecf4c6ba7dde822836feccb91bf92ed1c
rowida22 <rowida91200@gmail.com>
2023-05-18 00:41:49 +0300

    done

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
---comment 
+--comment
 SELECT tv_genres.name AS genre, COUNT(show_id) AS number_of_shows
 FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
