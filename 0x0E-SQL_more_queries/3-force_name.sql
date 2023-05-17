1b21ca9b3467c7f73fcdb4dd3933c398addeab4f
rowida22 <rowida91200@gmail.com>
2023-05-18 00:15:19 +0300

    T1

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- comment 
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- comment 
+SELECT id, name
+FROM cities
+WHERE state_id = (
+        SELECT id
+        FROM states
+        WHERE name = 'California'
+    );
