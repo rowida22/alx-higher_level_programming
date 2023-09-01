2840b9e366c079f69c005f972498594d03d9ea92
Rowida <rowida91200@gmail.com>
2023-09-01 15:35:14 +0300

    Test

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
+"""A script that sends a POST request with an email parameter to a URL
+   and displays the body of the response.
 """
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    if len(sys.argv) != 3:
+        print("Usage: ./6-post_email.py <URL> <email>")
+        sys.exit(1)
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {'email': email}
+
+    try:
+        response = requests.post(url, data=payload)
+        response.raise_for_status()  # Raise an exception if request fails
+        print(response.text)  # Print the body of the response
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
